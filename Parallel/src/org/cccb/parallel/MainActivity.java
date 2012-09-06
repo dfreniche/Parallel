45319260cd8f8057bf6232ac1dea75606ef81f3e
Diego Freniche <send.info@freniche.com>
2012-09-06 11:20:03 +0200

    Added code to listen to clicks in buttons. Added IDs to layout

--- Parallel/src/org/cccb/parallel/MainActivity.java
+++ Parallel/src/org/cccb/parallel/MainActivity.java
@@ -2,14 +2,45 @@ package org.cccb.parallel;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
 
 public class MainActivity extends Activity {
 
+	ImageButton buttonRoute;
+	ImageButton buttonCustomRoute;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        // link buttons
+        
+        this.buttonRoute = (ImageButton)findViewById(R.id.imageButtonRoute);
+        this.buttonCustomRoute = (ImageButton)findViewById(R.id.imageButtonCustomRoute);
+        
+        this.buttonRoute.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View arg0) {
+				Log.d("", "hello");
+				
+			}
+        	
+        });
+        
+        this.buttonCustomRoute.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View arg0) {
+				Log.d("", "hello");
+				
+			}
+        	
+        });
+        
     }
 
     @Override
