02d86d3c085ccc7aedb5e5d47cb17ee114d1a9ba
Diego Freniche <send.info@freniche.com>
2012-09-07 11:52:48 +0200

    Added code to check touch in list and get the right Route

--- Parallel/src/org/cccb/parallel/RoutesListActivity.java
+++ Parallel/src/org/cccb/parallel/RoutesListActivity.java
@@ -8,18 +8,24 @@ import org.cccb.parallel.model.Routes;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class RoutesListActivity extends ListActivity {
 
+	ListView list;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_routes_list);
         
         List <String> names = new ArrayList<String>();
-        List <Route> routes = (new Routes()).readAllRoutes().getAllRoutes();
+        final List <Route> routes = (new Routes()).readAllRoutes().getAllRoutes();
         for (Route r : routes) {
 			names.add(r.getName());
 			System.out.println(r.getName());
@@ -28,6 +34,15 @@ public class RoutesListActivity extends ListActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, names );
         
         setListAdapter(adapter);
+        
+        list = (ListView)findViewById(android.R.id.list);
+        list.setOnItemClickListener(new OnItemClickListener() {
+
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+				Log.d("",""+routes.get(arg2).getName());
+			}
+        	
+		});
     }
 
     @Override
